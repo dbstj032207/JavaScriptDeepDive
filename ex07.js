3a1d63d6ea758d85b6eb9d2c44ebde7a5bef0511
younseok <dbs032207@gmail.com>
2022-02-09 22:09:37 +0900

    07 end

--- ex07.js
+++ ex07.js
@@ -9,3 +9,30 @@ var x = 1;
 x++;
 
 console.log(x);
+
+x--;
+console.log(x);
+
+
+
+var x = 5, result;
+
+//선할당 후증가
+result = x++; 
+
+console.log(result, x);
+
+
+//선증가 후할당
+result = ++x;
+console.log(result, x);
+
+//선할당 후감소
+result = x--;
+console.log(result, x);
+
+//선감소 후할당
+result = --x;
+console.log(result, x);
+
+
