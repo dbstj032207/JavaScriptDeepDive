fd838a85f39632a5123be33984363406ca24d9f9
younseok <dbs032207@gmail.com>
2022-02-09 21:57:39 +0900

    06 end

--- ex06.js
+++ ex06.js
@@ -140,3 +140,31 @@ var obj = {};
 obj[key] = 'value';
 console.log(obj[key]);
 
+
+
+var foo;
+console.log(typeof(foo));
+
+foo = 3;
+console.log(typeof(foo));
+
+foo = 'string';
+console.log(typeof(foo));
+
+foo = true
+console.log(typeof(foo));
+
+foo = null;
+console.log(typeof(foo));
+
+foo = Symbol();
+console.log(typeof(foo));
+
+foo = {};
+console.log(typeof(foo));
+
+foo = [];
+console.log(typeof(foo));
+
+foo = function(){};
+console.log(typeof(foo));
