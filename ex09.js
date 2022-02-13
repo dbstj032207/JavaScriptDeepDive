b96615a912653b33c7404382847f931f1a573a58
younseok <dbs032207@gmail.com>
2022-02-13 22:44:02 +0900

    09-3 end

--- ex09.js
+++ ex09.js
@@ -62,12 +62,72 @@ console.log(Array + '');
 // 숫자 타입 => 문자열 타입
 String(1);          // "1"
 String(NaN);        // "NaN"
-String(Infinity)    // "Infinity"
+String(Infinity);    // "Infinity"
+
+// 불리언 타입 => 문자열 타입
+String(true);        // "true"
+String(false);       // "false"
 
 // 2. Object.prototype.toSring 메서드를 이용하는 방법
 (1).toString();     // "1"
 //...
 
 // 3. 문자열 연결 연산자를 이용하는 방법
+1 + '';
+//...
+
+// 9.3.2 숫자 타입으로 변환
+// 1.Number 생성자 합수를 new연산자 없이 호출하는 방법
+// 문자열 타입 => 숫자 타입
+Number('0');        // 0
+Number('-1');       // -1
+Number('10.53');    // 10.53
+
+// 불리언 타입 => 숫자 타입
+Number(true);       // 1
+Number(false);      // 0
+
+// 2. parseInt, parseFloat 함수를 사용하는 방법(문자열만 변환 가능)
+
+parseInt('0');
+// ...
+ 
+// 3. + 단항 산술 연산자를 이용하는 방법
++'0';
+// ...
+
+// 4. * 산술 연산자를 이용하는 방법
+'0' * 1;
+// ...
 
 
+// 9.3.3 불리언 타입으로 변환
+// 1. Boolean 생성자 함수를 new 연산자 없이 호출하는 방법
+// 문자열 타입 -> 불리언 타입
+Boolean('x');       // true
+Boolean('0');       // true
+Boolean('');         // false
+Boolean('false');    // true
+
+// 숫자 타입 => 불리언 타입
+Boolean(0);          // false
+Boolean(1);         // true
+Boolean(NaN);        // false
+Boolean(Infinity);   // true
+
+// null 타입
+Boolean(null);       // false
+
+// undefined 타입
+Boolean(undefined);  // false
+
+//객체 타입
+Boolean({});         // true
+Boolean([]);         // true
+
+
+// 2. ! 부정 논리 연산자를 두 번 사용하는 방법
+!!'x';
+//...
+
+/// 9.4 단축 평가
